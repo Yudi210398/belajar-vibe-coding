003bf1cd5969a1e784ca3b8973a63abd81bade57
Yudi Runat <yudi.berland@gmail.com>
2026-03-23 21:42:56 +0700

    feat: bootstrap NestJS app with Prisma 7

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = Number(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(port);
+}
+
+void bootstrap();
